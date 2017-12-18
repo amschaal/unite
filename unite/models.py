4c91529d3ed8d352d04e87d7ab9a9e897a642f3f
Adam Schaal <amschaal@gmail.com>
2017-12-18 13:03:32 -0800

    Make models more generic for virtually any kind of resource (not just users or groups)!

--- unite/models.py
+++ unite/models.py
@@ -1,14 +1,24 @@
 from django.db import models
-from django.contrib.auth.models import Group, User
+from django.contrib.postgres.fields.jsonb import JSONField
+import uuid
+
+class ResourceType(models.Model):
+    id = models.CharField(max_length=25,primary_key=True)
+    name = models.CharField(max_length=50)
+    description = models.TextField(blank=True)
+
+class Resource(models.Model):
+    identifier = models.CharField(max_length=32,default=uuid.uuid4)
+    type = models.ForeignKey(ResourceType)
+    data = JSONField()
+    class Meta:
+        unique_together = (('type', 'identifier'),)
 
 class Uniter(models.Model):
+    resource = models.ForeignKey(Resource)
     app_id = models.CharField(max_length=50)
     external_id = models.CharField(max_length=100)
+    data = JSONField(null=True)
     class Meta:
         abstract = True
-
-class GroupUniter(Uniter):
-    group = models.ForeignKey(Group)
-
-class UserUniter(Uniter):
-    user = models.ForeignKey(User)
+        unique_together = (('resource', 'app_id'),)
